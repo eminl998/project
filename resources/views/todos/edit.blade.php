302ed4d29d27bb3a0503d8dc6768eff4c1a7e428
Emin Lika <eminlika998@gmail.com>
2023-02-08 16:06:55 +0100

    08.02.2023

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -9,6 +9,15 @@
 
                 <div class="card-body">
                     <h4>Edit Form</h4>
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
 
                     <form method="POST" action="{{route('update', ['id' => $todo->id])}}">
                         @csrf
