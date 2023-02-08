eefb483d8471ba95323bcc0f25949a5a36bdfbe1
Emin Lika <eminlika998@gmail.com>
2023-02-08 15:45:56 +0100

    08.01.2023

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -11,37 +11,37 @@
                     <h4>Edit Form</h4>
 
                     <form method="POST" action="{{route('update', ['id' => $todo->id])}}">
-
                         @csrf
                         @method('PUT')
                         <input type="hidden" name="todo_id" value="{{$todo->id}}">
                         <div class="mb-3">
                           <label class="form-label">Title</label>
-                          <input type="text" name="title" class="form-control" value="{{$todo->title}}">
+                          <input type="text" name="title" class="form-control" value="{{ old('title') ? old('title') : $todo->title }}">
                         </div>
                         <div class="mb-3">
                           <label class="form-label">Description</label>
-                         <textarea name="description" class="form-control" cols="5" rows="5">{{$todo->description}}</textarea>
+                          <textarea name="description" class="form-control" cols="5" rows="5">{{ old('description') ? old('description') : $todo->description }}</textarea>
                         </div>
                         <div class="mb-3">
-                            <label for="">Task Level</label>
-                            <select name="task_level" class="form-control">
-                                <option disabled selected> Select Option</option>
-                                <option value="0" {{ $todo->task_level == 1 ? 'selected' : '' }}>Low</option>
-                                <option value="1" {{ $todo->task_level == 2 ? 'selected' : '' }}>Medium</option>
-                                <option value="2" {{ $todo->task_level == 3 ? 'selected' : '' }}>Urgent</option>
-                            </select>
+                          <label for="">Task Level</label>
+                          <select name="task_level" class="form-control">
+                            <option disabled> Select Option</option>
+                            <option value="0" {{ old('task_level') == 0 ? 'selected' : ($todo->task_level == 0 ? 'selected' : '') }}>Low</option>
+                            <option value="1" {{ old('task_level') == 1 ? 'selected' : ($todo->task_level == 1 ? 'selected' : '') }}>Medium</option>
+                            <option value="2" {{ old('task_level') == 2 ? 'selected' : ($todo->task_level == 2 ? 'selected' : '') }}>Urgent</option>
+                          </select>
                         </div>
                         <div class="mb-3">
-                            <label for="">Status</label>
-                            <select name="is_completed" class="form-control">
-                                <option disabled selected> Select Option</option>
-                                <option value="1">Completed</option>
-                                <option value="0">Incompleted</option>
-                            </select>
+                          <label for="">Status</label>
+                          <select name="is_completed" class="form-control">
+                            <option disabled> Select Option</option>
+                            <option value="1" {{ old('is_completed') == 1 ? 'selected' : ($todo->is_completed == 1 ? 'selected' : '') }}>Completed</option>
+                            <option value="0" {{ old('is_completed') == 0 ? 'selected' : ($todo->is_completed == 0 ? 'selected' : '') }}>Incompleted</option>
+                          </select>
                         </div>
                         <button type="submit" class="btn btn-primary">Update</button>
-                     </form>
+                      </form>
+                      
                     
                 </div>
             </div>
